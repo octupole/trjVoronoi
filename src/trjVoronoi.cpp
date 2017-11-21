333054421f75c052dade4236b9d6f301b749378a
Massimo Marchi <mm148881@gmail.com>
2017-11-21 23:58:39 +0100

    Add copyright notice

--- src/trjVoronoi.cpp
+++ src/trjVoronoi.cpp
@@ -1,65 +1,53 @@
 //============================================================================
 // Name        : trjVoronoi.cpp
 // Author      : Massimo Marchi
-// Version     :
+// Version     : 0.2alpha
 // Copyright   : CeCILL copyright
-// Description : Hello World in C++, Ansi-style
 //============================================================================
-/*! \mainpage trjSaxs
+/*! \mainpage trjVoronoi
  *
  * \section intro_sec Overview
- *	trjSaxs computes the small angle X-ray scattering (SAXS) intensity of
- *	solvated globular and membrane proteins from 3D fast Fourier
- *	transforms (FFT). It combines a suitable particle meshing
- *	interpolation, similar to the one use in smooth particle mesh Ewald
- *	for electrostatics, with a uniform solvent density FFT padding scheme
- *	to obtain a convenient SAXS spectral resolution. The CPU time scaling
- *	of the method, as a function of system size is highly favorable and
- *	its application to solution of large systems such as solvated membrane
- *	proteins is undemanding. Differently from other approaches, all
- *	contributions from the simulation cell are included. This means that
- *	the subtraction of the buffer from the solution scattering intensity
- *	is straightforward and devoid of artefact due to ad hoc definitions of
- *	proximal and distal solvent intensity contributions.
  *
- *	For the time being, the provided documentation is not comprehensive, but
- *	it will be upgraded in the future.
  *
  * \section install_sec Installation
  *
- *	trjSaxs uses a standard autoconf script and makefiles created by
+ *	trjVornoi uses a standard autoconf script and makefiles created by
  *	automake, like most GNU programs. This means your normal installation
- *	actions will be limited to run a ./configure, a makeand a make
- *	install. The code is dependent on the following packages:
- *
- *	fftw3 for discrete Fourier transforms A modified version of the
- *	xdrfile library, which is provided with the distribution They must be
- *	installed on an accessible directory of your system before the package
- *	installation can begin. As for the xdrfile package, it is contained in
- *	the directory $SOURCE/xdrfile-src, where $SOURCE is the directory
- *	where the trjSaxs source is. After you have changed directory to
- *	$SOURCE/xdrfile-src, the sequence of commands:
+ *	actions will be limited to run a
+ *	sh bootstrap (generates all autoconf file needed)
  *
  *	./configure
  *	make
  *	make install
  *
- *	will install the xdrfile libraries and includes needed by trjSaxs in
- *	$HOME/xdr. This can be changed to a different directory with --prefix,
- *	but the installation of trjSaxs is simpler if you do not change the
- *	xdrfile root directory.  Following the installation of the xdr
- *	package, you should return to the installation directory and issue a
- *	./configure. The command ./configure --help will tell what are the
- *	options available for compilation. Noticeably ./configure has amongst
- *	others the following options:
+ *	The code is dependent on the following packages:
  *
- *	--enable-intel		compile trjSaxs without user-define literals to  *				compile on an intel compiler
- *	--enable-openmp		compile trjSaxs with openmp support
- *	--enable-mpi		compile trjSaxs with MPI support
- *	--with-xdr-dir=dir	Provide the xdrfile root directory [$HOME/xdr]
- *	--with-fftw3-dir=dir	Provide the fftw3 root directory [/usr/local]
+ *	i)  A modified xdrfile library
+ *	ii) The voro++ library.
  *
- *	trjSaxs uses several features available with the C++11 standard, thus
+ *	Both are provided with this distribution, but voro++ is integrated
+ *	with the main code, whereas xdrfile need to be compiled separately.
+ *
+ *	Before installing trjVoronoi, go to the xdrfile directory and issue:
+ *
+ *	sh bootstrap
+ *	./configure
+ *	make install
+ *
+ *	Following the installation of the xdr package, return to the installation
+ *	directory and issue a
+ *	sh bootstrap
+ *	./configure
+ *	make
+ *	make install
+ *
+ *	Noticeably ./configure has amongst others the following options:
+ *
+ *	--enable-intel		compile trjVoronoi without user-define literals to
+ *					    compile on an intel compiler
+ *	--enable-mpi		compile trjVoronoi with MPI support
+ *
+ *	trjVoronoi uses several features available with the C++11 standard, thus
  *	it will not compile with GNU g++ compilers earlier than 4.8 and with
  *	Intel icpc compilers versions earlier than 15.0.2. It has been
  *	installed on Centos and Debian Linux machines and on Mac OS X.
