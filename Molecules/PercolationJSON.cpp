a112cd52eb5c23765c40c566ac0c022bce391b69
Massimo Marchi <mm148881@gmail.com>
2018-01-10 15:39:39 +0100

    Now use unique hashtag

--- Molecules/PercolationJSON.cpp
+++ Molecules/PercolationJSON.cpp
@@ -20,22 +20,23 @@ void PercolationJSON<T>::__Writeit(ostream & fout){
 	for(size_t o=0;o<this->Clusters.size();o++){
 		json myClust;
 		mapRes.clear();mapResAtm.clear();
-		std::map<string,vector<int>> Tag;
+		vector<int> Tag;
 		for(size_t p=0;p<this->Clusters[o].size();p++){
 			int n=this->Clusters[o][p];
 			mapRes[this->pResn[n]]++;
-			Tag[this->pResn[n]].push_back(n);
+			Tag.push_back(n);
 			for(size_t q{0};q<this->Atoms[n].size();q++){
 				mapResAtm[this->pResn[n]]++;
 			}
 		}
 		json tmp;
 		for(auto it{mapRes.begin()};it!= mapRes.end();it++){
-			std::stringstream ss;
-			std::sort(Tag[it->first].begin(),Tag[it->first].end());
-			std::copy(Tag[it->first].begin(),Tag[it->first].end(),std::ostream_iterator<int>( ss," "));
-			tmp[it->first]={it->second,mapResAtm[it->first],str_hash(ss.str())};
+			tmp[it->first]={it->second,mapResAtm[it->first]};
 		}
+		std::stringstream ss;
+		std::sort(Tag.begin(),Tag.end());
+		std::copy(Tag.begin(),Tag.end(),std::ostream_iterator<int>( ss," "));
+		tmp["hsh"]=str_hash(ss.str());
 		myJson["cluster"].push_back(tmp);
 	}
 
